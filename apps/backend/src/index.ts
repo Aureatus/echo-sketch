e5567de9cf0f2557aee3abfa2add55e5c1317b61
Alex Knott <alextknott@gmail.com>
2025-04-12 22:44:45 +0100

    Install zod +  hono zod validator middleware and create base testing post route

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,11 +1,25 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
 
 const app = new Hono();
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
-});
+}).post(
+	'/testing',
+	zValidator(
+	  'form',
+	  z.object({
+		body: z.string(),
+	  })
+	),
+	(c) => {
+	  const validated = c.req.valid('form')
+	  return c.json(validated)
+	}
+  )
 
 serve(
 	{
